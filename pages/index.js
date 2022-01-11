7953b4cab6547b0dde9e35f217ae3ee54b29d8b6
Florian Tischler <Florian.Tischler@gmx.net>
2022-01-11 10:32:47 +0100

    Add next-pwa

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,19 @@ export default function Home() {
         <meta name="theme-color" content="#289C38" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="manifest.json" />
+        <meta
+          name="application-name"
+          content="Landfrauen Remshalden-Grunbach"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Landfrauen Remshalden-Grunbach"
+        />
+        <meta name="description" content="Landfrauen Remshalden-Grunbach" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
